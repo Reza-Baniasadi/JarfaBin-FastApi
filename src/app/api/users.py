f88c1fe865fefbbe0719cb2abf1c8c96f88bd1f9
Mac <redsbaniasadi1381@gmail.com>
2024-09-05 14:41:43 +0000

    add create user api

---
+++ src/app/api/users.py
@@ -0,0 +1,43 @@
+from typing import Annotated, Any, cast
+
+from fastapi import APIRouter, Depends, Request
+from fastcrud.paginated import PaginatedListResponse, compute_offset, paginated_response
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from ...api.dependencies import get_current_superuser, get_current_user
+from ...core.db.database import async_get_db
+from ...core.exceptions.http_exceptions import DuplicateValueException, ForbiddenException, NotFoundException
+from ...core.security import blacklist_token, get_password_hash, oauth2_scheme
+from ...crud.crud_rate_limit import crud_rate_limits
+from ...crud.crud_tier import crud_tiers
+from ...crud.crud_users import crud_users
+from ...schemas.tier import TierRead
+from ...schemas.user import UserCreate, UserCreateInternal, UserRead, UserTierUpdate, UserUpdate
+
+router = APIRouter(tags=["users"])
+
+
+@router.post("/user", response_model=UserRead, status_code=201)
+async def write_user(
+    request: Request, user: UserCreate, db: Annotated[AsyncSession, Depends(async_get_db)]
+) -> UserRead:
+    email_row = await crud_users.exists(db=db, email=user.email)
+    if email_row:
+        raise DuplicateValueException("Email is already registered")
+
+    username_row = await crud_users.exists(db=db, username=user.username)
+    if username_row:
+        raise DuplicateValueException("Username not available")
+
+    user_internal_dict = user.model_dump()
+    user_internal_dict["hashed_password"] = get_password_hash(password=user_internal_dict["password"])
+    del user_internal_dict["password"]
+
+    user_internal = UserCreateInternal(**user_internal_dict)
+    created_user = await crud_users.create(db=db, object=user_internal)
+
+    user_read = await crud_users.get(db=db, id=created_user.id, schema_to_select=UserRead)
+    if user_read is None:
+        raise NotFoundException("Created user not found")
+
+    return cast(UserRead, user_read)

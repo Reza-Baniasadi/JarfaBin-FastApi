53e9825df692711fb8eb9fe42f3f544e08f76a3f
Mac <redsbaniasadi1381@gmail.com>
2024-12-17 10:05:20 +0000

    create function for check rate limit user

--- src/app/utils/Redis.py
+++ src/app/utils/Redis.py
@@ -34,3 +34,28 @@ class RateLimiter:
     #         logger.error("Redis client is not initialized.")
     #         raise Exception("Redis client is not initialized.")
     #     return instance.client
+
+        async def is_rate_limited(self, db: AsyncSession, user_id: int, path: str, limit: int, period: int) -> bool:
+        client = self.get_client()
+        current_timestamp = int(datetime.now(UTC).timestamp())
+        window_start = current_timestamp - (current_timestamp % period)
+
+        sanitized_path = sanitize_path(path)
+        key = f"ratelimit:{user_id}:{sanitized_path}:{window_start}"
+
+        try:
+            current_count = await client.incr(key)
+            if current_count == 1:
+                await client.expire(key, period)
+
+            if current_count > limit:
+                return True
+
+        except Exception as e:
+            logger.exception(f"Error checking rate limit for user {user_id} on path {path}: {e}")
+            raise e
+
+        return False
+
+
+rate_limiter = RateLimiter()

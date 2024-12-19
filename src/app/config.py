3490ab4b744350ced8b78d670d9f408aeadce6a0
Mac <redsbaniasadi1381@gmail.com>
2024-12-19 12:52:01 +0000

    add app setting class and get status from .env

--- src/app/config.py
+++ src/app/config.py
@@ -8,3 +8,13 @@ from starlette.config import Config
 current_file_dir = os.path.dirname(os.path.realpath(__file__))
 env_path = os.path.join(current_file_dir, "..", "..", ".env")
 config = Config(env_path)
+
+
+class AppSettings(BaseSettings):
+    APP_NAME: str = config("APP_NAME", default="FastAPI app")
+    APP_DESCRIPTION: str | None = config("APP_DESCRIPTION", default=None)
+    APP_VERSION: str | None = config("APP_VERSION", default=None)
+    LICENSE_NAME: str | None = config("LICENSE", default=None)
+    CONTACT_NAME: str | None = config("CONTACT_NAME", default=None)
+    CONTACT_EMAIL: str | None = config("CONTACT_EMAIL", default=None)
+

42a7d97a67fbb8cee60a071baa0d824e224c9e03
Mac <redsbaniasadi1381@gmail.com>
2024-12-29 08:56:00 +0000

    create fuction for run all database and craete table

---
+++ src/app/setup.py
@@ -0,0 +1,42 @@
+from collections.abc import AsyncGenerator, Callable
+from contextlib import _AsyncGeneratorContextManager, asynccontextmanager
+from typing import Any
+
+import anyio
+import fastapi
+import redis.asyncio as redis
+from arq import create_pool
+from arq.connections import RedisSettings
+from fastapi import APIRouter, Depends, FastAPI
+from fastapi.openapi.docs import get_redoc_html, get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
+
+from ..api.dependencies import get_current_superuser
+from ..core.utils.rate_limit import rate_limiter
+from ..middleware.client_cache_middleware import ClientCacheMiddleware
+from ..models import *  # noqa: F403
+from .config import (
+    AppSettings,
+    ClientSideCacheSettings,
+    DatabaseSettings,
+    EnvironmentOption,
+    EnvironmentSettings,
+    RedisCacheSettings,
+    RedisQueueSettings,
+    RedisRateLimiterSettings,
+    settings,
+)
+from .db.database import Base
+from .db.database import async_engine as engine
+from .utils import cache, queue
+
+
+async def create_tables() -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def create_redis_cache_pool() -> None:
+    cache.pool = redis.ConnectionPool.from_url(settings.REDIS_CACHE_URL)
+    cache.client = redis.Redis.from_pool(cache.pool)  # type: ignore
+

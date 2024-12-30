89cd672d1d5c94acbb8ac46de6aa90f97860ea16
Mac <redsbaniasadi1381@gmail.com>
2024-12-30 11:03:00 +0000

    add application function and create 4 route and used instance and control swagger and redoc

--- src/app/setup.py
+++ src/app/setup.py
@@ -108,3 +108,65 @@ def lifespan_factory(
                 await close_redis_rate_limit_pool()
 
     return lifespan
+
+
+def create_application(
+    router: APIRouter,
+    settings: (
+        DatabaseSettings
+        | RedisCacheSettings
+        | AppSettings
+        | ClientSideCacheSettings
+        | RedisQueueSettings
+        | RedisRateLimiterSettings
+        | EnvironmentSettings
+    ),
+    create_tables_on_start: bool = True,
+    lifespan: Callable[[FastAPI], _AsyncGeneratorContextManager[Any]] | None = None,
+    **kwargs: Any,
+) -> FastAPI:
+
+    if isinstance(settings, AppSettings):
+        to_update = {
+            "title": settings.APP_NAME,
+            "description": settings.APP_DESCRIPTION,
+            "contact": {"name": settings.CONTACT_NAME, "email": settings.CONTACT_EMAIL},
+            "license_info": {"name": settings.LICENSE_NAME},
+        }
+        kwargs.update(to_update)
+
+    if isinstance(settings, EnvironmentSettings):
+        kwargs.update({"docs_url": None, "redoc_url": None, "openapi_url": None})
+
+    if lifespan is None:
+        lifespan = lifespan_factory(settings, create_tables_on_start=create_tables_on_start)
+
+    application = FastAPI(lifespan=lifespan, **kwargs)
+    application.include_router(router)
+
+    if isinstance(settings, ClientSideCacheSettings):
+        application.add_middleware(ClientCacheMiddleware, max_age=settings.CLIENT_CACHE_MAX_AGE)
+
+    if isinstance(settings, EnvironmentSettings):
+        if settings.ENVIRONMENT != EnvironmentOption.PRODUCTION:
+            docs_router = APIRouter()
+            if settings.ENVIRONMENT != EnvironmentOption.LOCAL:
+                docs_router = APIRouter(dependencies=[Depends(get_current_superuser)])
+
+            @docs_router.get("/docs", include_in_schema=False)
+            async def get_swagger_documentation() -> fastapi.responses.HTMLResponse:
+                return get_swagger_ui_html(openapi_url="/openapi.json", title="docs")
+
+            @docs_router.get("/redoc", include_in_schema=False)
+            async def get_redoc_documentation() -> fastapi.responses.HTMLResponse:
+                return get_redoc_html(openapi_url="/openapi.json", title="docs")
+
+            @docs_router.get("/openapi.json", include_in_schema=False)
+            async def openapi() -> dict[str, Any]:
+                out: dict = get_openapi(title=application.title, version=application.version, routes=application.routes)
+                return out
+
+            application.include_router(docs_router)
+
+    return application
+

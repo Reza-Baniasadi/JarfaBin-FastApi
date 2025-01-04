8cf9ff0499693c9a9c0ad5bdf538a6c4f3ce0fae
Mac <redsbaniasadi1381@gmail.com>
2025-01-04 09:04:25 +0000

    add dto postread

--- src/app/Schema/post.py
+++ src/app/Schema/post.py
@@ -18,3 +18,15 @@ class Post(TimestampSchema, PostBase, UUIDSchema, PersistentDeletion):
         Field(pattern=r"^(https?|ftp)://[^\s/$.?#].[^\s]*$", examples=["https://www.postimageurl.com"], default=None),
     ]
     created_by_user_id: int
+
+
+    class PostRead(BaseModel):
+    id: int
+    title: Annotated[str, Field(min_length=2, max_length=30, examples=["This is my post"])]
+    text: Annotated[str, Field(min_length=1, max_length=63206, examples=["This is the content of my post."])]
+    media_url: Annotated[
+        str | None,
+        Field(examples=["https://www.postimageurl.com"], default=None),
+    ]
+    created_by_user_id: int
+    created_at: datetime

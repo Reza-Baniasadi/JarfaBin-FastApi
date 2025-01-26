c171e62515a0add955acbb45cf21d7620630c761
Mac <redsbaniasadi1381@gmail.com>
2025-01-26 10:30:00 +0000

    add create crypto and with postman

--- src/app/crud/crud_crypto.py
+++ src/app/crud/crud_crypto.py
@@ -7,3 +7,11 @@ def get_crypto(db: Session, crypto_id: int):
 
 def get_crypto_by_symbol(db: Session, symbol: str):
     return db.query(models.CryptoCurrency).filter(models.CryptoCurrency.symbol == symbol).first()
+
+
+def create_crypto(db: Session, crypto: schemas.CryptoCurrencyCreate):
+    db_crypto = models.CryptoCurrency(name=crypto.name, symbol=crypto.symbol, price_usd=0)
+    db.add(db_crypto)
+    db.commit()
+    db.refresh(db_crypto)
+    return db_crypto

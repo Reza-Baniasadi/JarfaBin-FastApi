24c30efe69175a9eb673da88b5d032bd0b4ab652
Mac <redsbaniasadi1381@gmail.com>
2025-01-26 10:07:00 +0000

    get crypto bt name

--- src/app/crud/crud_crypto.py
+++ src/app/crud/crud_crypto.py
@@ -3,3 +3,7 @@ from . import models, schemas
 
 def get_crypto(db: Session, crypto_id: int):
     return db.query(models.CryptoCurrency).filter(models.CryptoCurrency.id == crypto_id).first()
+
+
+def get_crypto_by_symbol(db: Session, symbol: str):
+    return db.query(models.CryptoCurrency).filter(models.CryptoCurrency.symbol == symbol).first()

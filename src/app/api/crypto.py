a699de4e23b35bc62244798135c7a9674a5481c2
Mac <redsbaniasadi1381@gmail.com>
2025-01-28 12:07:00 +0000

    add route for get crypto by symbol and test with postman

---
+++ src/app/api/crypto.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+
+from app.db.session import get_db
+from . import crud, schemas, tasks
+
+router = APIRouter(prefix="/crypto", tags=["Crypto"])
+
+@router.post("/", response_model=schemas.CryptoCurrency)
+def create_crypto(crypto: schemas.CryptoCurrencyCreate, db: Session = Depends(get_db)):
+    db_crypto = crud.get_crypto_by_symbol(db, crypto.symbol)
+    if db_crypto:
+        raise HTTPException(status_code=400, detail="Crypto already exists")
+    return crud.create_crypto(db, crypto)

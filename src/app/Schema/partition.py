47acb53b996f8638e7ac06f09f2e2d59aecbb9c3
Mac <redsbaniasadi1381@gmail.com>
2025-02-01 08:25:00 +0000

    add schema for partition

--- src/app/Schema/partition.py
+++ src/app/Schema/partition.py
@@ -15,3 +15,8 @@ class PartitionOut(PartitionBase):
     updated_at: datetime
     size_bytes: int
     record_count: int
+
+class PartitionUpdate(BaseModel):
+    description: Optional[str] = None
+    size_bytes: Optional[int] = None
+    record_count: Optional[int] = None

54897a2ea1a297e2229ce5ce1f27c13189669ea6
Mac <redsbaniasadi1381@gmail.com>
2025-02-02 08:28:00 +0000

    add post router

--- src/app/api/partition.py
+++ src/app/api/partition.py
@@ -3,3 +3,14 @@ from sqlalchemy.orm import Session
 from models import Partition
 from schemas import PartitionCreate, PartitionOut, PartitionUpdate
 from database import get_db
+
+
+router = APIRouter()
+
+@router.post("/", response_model=PartitionOut)
+def create_partition(payload: PartitionCreate, db: Session = Depends(get_db)):
+    db_partition = Partition(name=payload.name, description=payload.description)
+    db.add(db_partition)
+    db.commit()
+    db.refresh(db_partition)
+    return db_partition

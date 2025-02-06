9a46f7ba17ca38483e26f3a25940b20c9beadf48
Mac <redsbaniasadi1381@gmail.com>
2025-02-06 08:04:00 +0000

    add post router

--- src/app/api/accounting.py
+++ src/app/api/accounting.py
@@ -3,3 +3,13 @@ from sqlalchemy.orm import Session
 from accounting_models import Transaction
 from accounting_schemas import TransactionCreate, TransactionOut
 from accounting_database import get_db
+
+router = APIRouter()
+
+@router.post("/", response_model=TransactionOut)
+def create_transaction(payload: TransactionCreate, db: Session = Depends(get_db)):
+    db_transaction = Transaction(**payload.dict())
+    db.add(db_transaction)
+    db.commit()
+    db.refresh(db_transaction)
+    return db_transaction

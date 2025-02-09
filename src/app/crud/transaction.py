2c331a6f7eb1c669f9878c9a6b05f8530c0c3036
Mac <redsbaniasadi1381@gmail.com>
2025-02-09 11:22:00 +0000

    get vall transaction

--- src/app/crud/transaction.py
+++ src/app/crud/transaction.py
@@ -13,3 +13,7 @@ def create_transaction(db: Session, transaction: schemas.TransactionCreate):
     db.commit()
     db.refresh(db_transaction)
     return db_transaction
+
+
+def get_transactions(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Transaction).offset(skip).limit(limit).all()

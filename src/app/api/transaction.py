9e86b777dba7638452ad42af1716eac2c7fc97e1
Mac <redsbaniasadi1381@gmail.com>
2025-02-10 08:28:00 +0000

    add router transaction

--- src/app/api/transaction.py
+++ src/app/api/transaction.py
@@ -7,3 +7,9 @@ router = APIRouter()
 @router.post("/transactions/", response_model=schemas.TransactionResponse)
 async def create_transaction(transaction: schemas.TransactionCreate, db: Session = Depends(database.get_db)):
     return crud.create_transaction(db=db, transaction=transaction)
+
+
+
+@router.get("/transactions/", response_model=list[schemas.TransactionResponse])
+async def get_transactions(skip: int = 0, limit: int = 100, db: Session = Depends(database.get_db)):
+    return crud.get_transactions(db=db, skip=skip, limit=limit)

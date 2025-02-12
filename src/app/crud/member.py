75d429931b7684b48cf005df1abd77129a690039
Mac <redsbaniasadi1381@gmail.com>
2025-02-12 08:28:00 +0000

    add import for member

---
+++ src/app/crud/member.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import Session
+from models import Member
+from schemas import MemberCreate, MemberUpdate
+from typing import List
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+import sys
+sys.path.append(r'D:/API ENV/crud.py') 
+import schemas,models
+from pymysql.err import IntegrityError

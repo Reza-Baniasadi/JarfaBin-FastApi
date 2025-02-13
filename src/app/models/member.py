73e519a4ceebc0de648973c04858ee684362f77d
Mac <redsbaniasadi1381@gmail.com>
2025-02-13 09:08:00 +0000

    add  class model memeber

--- src/app/models/member.py
+++ src/app/models/member.py
@@ -2,3 +2,19 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String,ForeignKey,Boolean,Float,DateTime
 from database import Base
 from sqlalchemy import event
+
+
+class Member(Base):
+    __tablename__ = "Members"
+
+    Member_id = Column(Integer,primary_key= True,index = True)
+    First_name = Column(String(20))
+    Middle_name = Column(String(20))  #optional Attribute
+    Last_name = Column(String(20))
+    Email = Column(String(150), index=True,unique=True, nullable=False)
+    Country_Id = Column(Integer, ForeignKey("Country_Info.Country_Id", onupdate='CASCADE', ondelete='CASCADE'))
+    Contact_Number = Column(String(20),unique=True, nullable=False)
+    username = Column(String(50), unique=True, index=True)
+    password = Column(String(50))
+    account_status = Column(Integer)
+    processed_by_id = Column(Integer, ForeignKey("Users.id", onupdate='CASCADE', ondelete='CASCADE'))

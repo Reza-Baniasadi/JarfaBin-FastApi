7b442e58b40954c8274757eb0aade747469aaa06
Mac <redsbaniasadi1381@gmail.com>
2025-02-15 10:08:00 +0000

    last chnage

--- src/app/models/member.py
+++ src/app/models/member.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String,ForeignKey,
+from sqlalchemy import Column, Integer, String,ForeignKey,Boolean,Float,DateTime
 from database import Base
+from sqlalchemy import event
 
 
 class Member(Base):

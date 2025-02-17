f92bae4103852fd1bce6f4f75cf38801ce0d5246
Mac <redsbaniasadi1381@gmail.com>
2025-02-17 01:05:00 +0000

    add update country

--- src/app/crud/country.py
+++ src/app/crud/country.py
@@ -28,3 +28,12 @@ def get_country_info(db: Session, country_id: int):
 
 def get_all_country_info(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Country_Info).offset(skip).limit(limit).all()
+
+def update_country_info(db: Session, country_id: int, country_info: schemas.update_country):
+    db_country_info = db.query(models.Country_Info).filter(models.Country_Info.Country_Id == country_id).first()
+    if db_country_info:
+        for attr, value in country_info.dict().items():
+            setattr(db_country_info, attr, value)
+        db.commit()
+        db.refresh(db_country_info)
+    return db_country_info

c87e04568a734fc94252ed9104c03e9b19edcc54
Mac <redsbaniasadi1381@gmail.com>
2025-03-02 08:52:00 +0000

    add class

--- src/app/utils/data.py
+++ src/app/utils/data.py
@@ -6,3 +6,17 @@ from typing import Dict, Iterable, List, Literal, Optional, Tuple
 
 import pandas as pd
 import numpy as np
+
+TimeCol = Literal["timestamp", "time", "date", "datetime"]
+PriceCols = List[str]
+
+
+@dataclass
+class CleanReport:
+rows_in: int
+rows_out: int
+duplicates_dropped: int
+cols_before: List[str]
+cols_after: List[str]
+inferred_ts_unit: Optional[str]
+warnings: List[str]

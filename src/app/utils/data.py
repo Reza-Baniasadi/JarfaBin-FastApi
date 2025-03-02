6c40fe8a6962fcc2ae2b5b47a0c4d0b4ce60e3f7
Mac <redsbaniasadi1381@gmail.com>
2025-03-02 08:52:00 +0000

    add import

---
+++ src/app/utils/data.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from io import BytesIO
+from typing import Dict, Iterable, List, Literal, Optional, Tuple
+
+
+import pandas as pd
+import numpy as np

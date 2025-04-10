2307173606603376964d4561b2e55bdd1e582b4f
Mac <redsbaniasadi1381@gmail.com>
2025-04-10 10:04:00 +0000

    set catch function

--- src/catch.py
+++ src/catch.py
@@ -2,3 +2,8 @@ import httpx
 import time
 
 client = httpx.AsyncClient(timeout=15.0)
+
+
+def set_cache(key: str, value: dict, ttl: int = 30):
+    expire_at = time.time() + ttl
+    _cache[key] = (value, expire_at)

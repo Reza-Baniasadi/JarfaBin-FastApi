f17ef874192a0a49bc4339a67228fc9124315f81
Mac <redsbaniasadi1381@gmail.com>
2025-04-10 12:04:00 +0000

    get catch

--- src/catch.py
+++ src/catch.py
@@ -9,3 +9,14 @@ _cache = {}
 def set_cache(key: str, value: dict, ttl: int = 30):
     expire_at = time.time() + ttl
     _cache[key] = (value, expire_at)
+
+
+def get_cache(key: str):
+    data = _cache.get(key)
+    if not data:
+        return None
+    value, expire_at = data
+    if time.time() > expire_at:
+        _cache.pop(key, None)
+        return None
+    return value

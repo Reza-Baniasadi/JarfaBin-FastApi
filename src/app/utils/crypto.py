10fcdb331f4c1a0862f24fe5afb6e9a8458cff64
Mac <redsbaniasadi1381@gmail.com>
2025-04-27 11:49:00 +0000

    prepare fastapi

--- src/app/utils/crypto.py
+++ src/app/utils/crypto.py
@@ -2,3 +2,6 @@ from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 from typing import Optional, Dict, Literal
+
+
+app = FastAPI(title="Crypto File Utils API", version="0.1.0")

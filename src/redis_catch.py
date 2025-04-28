0a784617a0afef75c9148b6a85516d60a2dfa80d
Mac <redsbaniasadi1381@gmail.com>
2025-04-28 02:35:00 +0000

    add logger

--- src/redis_catch.py
+++ src/redis_catch.py
@@ -4,3 +4,10 @@ import logging
 from config import settings
 
 redis_client = redis.StrictRedis.from_url(settings.REDIS_URL, decode_responses=True)
+
+logger = logging.getLogger("crypto_api")
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)

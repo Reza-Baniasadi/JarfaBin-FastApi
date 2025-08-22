3eb53f2861ad5a978d4b130070da5be6c9e0a4df
Mac <redsbaniasadi1381@gmail.com>
2025-08-22 19:25:13 +0330

    add dto for user delete and update intenal

---
+++ src/app/migrations/env.py
@@ -0,0 +1,29 @@
+import asyncio
+import importlib
+import pkgutil
+from logging.config import fileConfig
+
+from alembic import context
+from sqlalchemy import pool
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import async_engine_from_config
+
+from app.core.config import settings
+from app.core.db.database import Base
+
+
+config = context.config
+
+config.set_main_option(
+    "sqlalchemy.url",
+    f"{settings.POSTGRES_ASYNC_PREFIX}{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_SERVER}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}",
+)
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+
+def import_models(package_name):
+    package = importlib.import_module(package_name)
+    for _, module_name, _ in pkgutil.walk_packages(package.__path__, package.__name__ + "."):
+        importlib.import_module(module_name)

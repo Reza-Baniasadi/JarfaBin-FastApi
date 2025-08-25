a2dab9b9ed91f05ae29fa6211bc168bd0d84e18e
Mac <redsbaniasadi1381@gmail.com>
2025-08-25 02:26:00 +0000

    change ratye limit

--- src/app/models/rate_limit.py
+++ src/app/models/rate_limit.py
@@ -1,6 +1,6 @@
-from datetime import UTC, datetime
+from datetime import datetime, timezone
 
-from sqlalchemy import DateTime, ForeignKey, Integer, String
+from sqlalchemy import DateTime, ForeignKey, Integer, String, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from ..core.db.database import Base
@@ -9,12 +9,50 @@ from ..core.db.database import Base
 class RateLimit(Base):
     __tablename__ = "rate_limit"
 
-    id: Mapped[int] = mapped_column("id", autoincrement=True, nullable=False, unique=True, primary_key=True, init=False)
-    tier_id: Mapped[int] = mapped_column(ForeignKey("tier.id"), index=True)
-    name: Mapped[str] = mapped_column(String, nullable=False, unique=True)
-    path: Mapped[str] = mapped_column(String, nullable=False)
-    limit: Mapped[int] = mapped_column(Integer, nullable=False)
-    period: Mapped[int] = mapped_column(Integer, nullable=False)
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+        nullable=False,
+        unique=True,
+        init=False,
+    )
 
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default_factory=lambda: datetime.now(UTC))
-    updated_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), default=None)
+    tier_id: Mapped[int] = mapped_column(
+        ForeignKey("tier.id"),
+        index=True,
+        nullable=False,
+    )
+
+    name: Mapped[str] = mapped_column(
+        String,
+        nullable=False,
+        unique=True,
+    )
+
+    path: Mapped[str] = mapped_column(
+        String,
+        nullable=False,
+    )
+
+    limit: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False,
+    )
+
+    period: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False,
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default_factory=lambda: datetime.now(timezone.utc),
+        nullable=False,
+    )
+
+    updated_at: Mapped[datetime | None] = mapped_column(
+        DateTime(timezone=True),
+        onupdate=func.now(),
+        nullable=True,
+    )

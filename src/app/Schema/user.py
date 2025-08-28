717e2e74d5ae2daffe10612d1c8b8804d1db7f1a
Mac <redsbaniasadi1381@gmail.com>
2025-08-28 10:09:00 +0000

    schema user change

--- src/app/Schema/user.py
+++ src/app/Schema/user.py
@@ -1,42 +1,26 @@
 from datetime import datetime
 from typing import Annotated
 
-from pydantic import BaseModel, ConfigDict, EmailStr, Field
+from pydantic import BaseModel, ConfigDict, EmailStr, Field, HttpUrl
 
 from ..core.schemas import PersistentDeletion, TimestampSchema, UUIDSchema
 
 
 class UserBase(BaseModel):
-    name: Annotated[str, Field(min_length=2, max_length=30, examples=["User Userson"])]
-    username: Annotated[str, Field(min_length=2, max_length=20, pattern=r"^[a-z0-9]+$", examples=["userson"])]
-    email: Annotated[EmailStr, Field(examples=["user.userson@example.com"])]
-
-
-class User(TimestampSchema, UserBase, UUIDSchema, PersistentDeletion):
-    profile_image_url: Annotated[str, Field(default="https://www.profileimageurl.com")]
-    hashed_password: str
-    is_superuser: bool = False
-    tier_id: int | None = None
-
-
-class UserRead(BaseModel):
-    id: int
-
-    name: Annotated[str, Field(min_length=2, max_length=30, examples=["User Userson"])]
-    username: Annotated[str, Field(min_length=2, max_length=20, pattern=r"^[a-z0-9]+$", examples=["userson"])]
-    email: Annotated[EmailStr, Field(examples=["user.userson@example.com"])]
-    profile_image_url: str
-    tier_id: int | None
+    name: Annotated[str, Field(min_length=2, max_length=30, example="User Userson")]
+    username: Annotated[str, Field(min_length=2, max_length=20, pattern=r"^[a-z0-9]+$", example="userson")]
+    email: Annotated[EmailStr, Field(example="user.userson@example.com")]
 
 
 class UserCreate(UserBase):
+    password: Annotated[
+        str,
+        Field(
+            pattern=r"^.{8,}|[0-9]+|[A-Z]+|[a-z]+|[^a-zA-Z0-9]+$",
+            example="Str1ngst!"
+        )
+    ]
     model_config = ConfigDict(extra="forbid")
-
-    password: Annotated[str, Field(pattern=r"^.{8,}|[0-9]+|[A-Z]+|[a-z]+|[^a-zA-Z0-9]+$", examples=["Str1ngst!"])]
-
-
-class UserCreateInternal(UserBase):
-    hashed_password: str
 
 
 class UserCreateInternal(UserBase):
@@ -44,34 +28,29 @@ class UserCreateInternal(UserBase):
 
 
 class UserUpdate(BaseModel):
+    name: Annotated[str | None, Field(min_length=2, max_length=30, example="User Userberg", default=None)]
+    username: Annotated[str | None, Field(min_length=2, max_length=20, pattern=r"^[a-z0-9]+$", example="userberg", default=None)]
+    email: Annotated[EmailStr | None, Field(example="user.userberg@example.com", default=None)]
+    profile_image_url: Annotated[HttpUrl | None, Field(default=None, example="https://www.profileimageurl.com")]
     model_config = ConfigDict(extra="forbid")
 
-    name: Annotated[str | None, Field(min_length=2, max_length=30, examples=["User Userberg"], default=None)]
-    username: Annotated[
-        str | None, Field(min_length=2, max_length=20, pattern=r"^[a-z0-9]+$", examples=["userberg"], default=None)
-    ]
-    email: Annotated[EmailStr | None, Field(examples=["user.userberg@example.com"], default=None)]
-    profile_image_url: Annotated[
-        str | None,
-        Field(
-            pattern=r"^(https?|ftp)://[^\s/$.?#].[^\s]*$", examples=["https://www.profileimageurl.com"], default=None
-        ),
-    ]
 
-
-class UserUpdateInternal(UserUpdate):
+class UserUpdateInternal(UserUpdate, TimestampSchema):
     updated_at: datetime
+
+
+class UserRead(UserBase, UUIDSchema, TimestampSchema):
+    id: int
+    profile_image_url: HttpUrl
+    tier_id: int | None
 
 
 class UserTierUpdate(BaseModel):
     tier_id: int
 
 
-class UserDelete(BaseModel):
+class UserDelete(PersistentDeletion):
     model_config = ConfigDict(extra="forbid")
-
-    is_deleted: bool
-    deleted_at: datetime
 
 
 class UserRestoreDeleted(BaseModel):
